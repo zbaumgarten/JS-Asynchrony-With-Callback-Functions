86056a7d15e9cd87120414e9cb32348a4d35d9dc
Joshua Miles <joshua.m@hackerusa.com>
2021-04-16 21:48:33 -0500

    -

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Game IV
+
+This is an activity for FSWD Lesson 5.4.2- Practicing Async & Await
+
+Please refer to the Activity Guide in Canvas for directions.
---
+++ index.js
@@ -0,0 +1,15 @@
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+move(character).to(100, 250)
+
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
