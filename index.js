84d25b8c5bdf3c6c4544033898c31bc1d4c3893f
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-03-31 19:07:14 -0500

    commitment!

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+npc.walkNorth(1400, () => {
+    npc.walkEast(1200, () => {
+        npc.walkSouth(300, () => {
+            npc.walkEast(1500, () => {
+                npc.walkSouth(1500, () => {
+                    npc.walkWest(2700, () => {
+                        npc.walkNorth(400)
+                    })
+                })
+            })
+        })
+    })
+})
 
 // Create the inventory
 const inventory = newInventory()
